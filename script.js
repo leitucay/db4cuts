690362205020c427f5f114ba2335c4342a58231f
leitucay <157317035+leitucay@users.noreply.github.com>
2026-03-15 00:14:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,129 +1,32 @@
-/*
- * @file main.js
- * Created by hschmale on 3/6/16.
- */
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const sticker = document.getElementById('sticker');
+const startBtn = document.getElementById('startBtn');
+const intervalSelect = document.getElementById('interval');
+const downloadLink = document.getElementById('downloadLink');
 
-// How long to wait before snapping the picture
-const COUNTDOWNLENGTH = 5;
-const COUNTDOWN_TIMEOUT = COUNTDOWNLENGTH * 1000;
+// Start webcam
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => { video.srcObject = stream; })
+  .catch(err => console.error(err));
 
-var PhotoBooth = {
-    onMediaStream: function(stream) {
-        PhotoBooth.preview = $('canvas')[0];
+startBtn.addEventListener('click', () => {
+  const interval = parseInt(intervalSelect.value);
+  capturePhoto();
+  setTimeout(capturePhoto, interval);
+});
 
-        PhotoBooth.image = document.createElement('canvas');
-        PhotoBooth.image.width = 800;
-        PhotoBooth.image.height = 600;
+function capturePhoto() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  const ctx = canvas.getContext('2d');
 
-        PhotoBooth.localVideo = $('video')[0];
-        PhotoBooth.localVideo.src = window.URL.createObjectURL(stream);
-    },
-    noStream: function() {
-        console.log('FAIL TO GET WEBCAM ACCESS');
-    }
-};
+  // Draw video
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-function countdown(secs){
-    var countdownDiv = $('.countdown');
-    if(secs == 0){
-        countdownDiv.hide();
-        return;
-    }
-    countdownDiv.html(secs);
-    secs--;
-    setTimeout(function() {
-        countdown(secs);
-    }, 1000);
-}
+  // Draw sticker
+  ctx.drawImage(sticker, 0, 0, sticker.width, sticker.height);
 
-// shows countdown then captures the image at end of countdown
-function takePicture() {
-    $('.countdown').show();
-    countdown(COUNTDOWNLENGTH);
-    setTimeout(function() {
-        PhotoBooth.image.getContext('2d')
-            .drawImage(PhotoBooth.localVideo, 0, 0,
-                PhotoBooth.image.width,
-                PhotoBooth.image.height);
-        PhotoBooth.preview.getContext('2d')
-            .drawImage(PhotoBooth.image, 0, 0, 160, 120);
-        $('#preview').show();
-    }, COUNTDOWN_TIMEOUT);
-}
-
-// returns true if <value> exists in <arr>
-function existsInArray(arr, value){
-    for(i in arr){
-        if(value === arr[i]){
-            return true;
-        }
-    }
-    return false;
-}
-
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
-// uploads the image to the server
-function makeNewspaper() {
-    var dataUrl = PhotoBooth.image.toDataURL("image/jpeg");
-    var data = {
-        name: $('#username').val(),
-        email: $('#emailAddress').val(),
-        notes: $('#notes').val(),
-        caption: $('#caption').val(),
-        image: dataUrl
-    };
-    // validate inputs
-    if(data.name.length <= 1){
-        alert("Name Too Short. Enter longer name");
-        return;
-    }
-    data.email.split(',').forEach(function(email){
-        if(!validateEmail(email)){
-            alert("Email Invalid:" + email);
-            return;
-        }
-    });
-    /*
-    if(!existsInArray([1,2,3], parseInt(data.printControl))){
-        alert("Invalid Print Request");
-        return;
-    }
-    */
-    // upload it now because it has not failed
-    $.post(
-        '/api/upload.php',
-        data,
-        function(resp, status){
-            alert(resp + ' ' + status);
-            if(status == 200){
-                clearForm();
-            }
-        }
-    );
-}
-
-function clearForm(){
-    $('#username').val('');
-    $('#emailAddress').val('');
-    $('#notes').val('');
-    $('#caption').val('');
-}
-
-// init the program
-function init() {
-    // set up event listeners
-    $('#takePicture').click(takePicture);
-    $('#usePicture').click(makeNewspaper);
-    $('#clearForm').click(clearForm);
-
-    // open the video stream
-    getUserMedia(
-        {video: true},
-        PhotoBooth.onMediaStream,
-        PhotoBooth.noStream
-    );
+  // Update download link
+  downloadLink.href = canvas.toDataURL('image/png');
 }
